94f3ae8d97c74239a48401cb5b13d9ab274cbb60
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-04-22 20:20:58 +0530

    ok

---
+++ app/ui/src/pages/user/onboard/index.js
@@ -0,0 +1,15 @@
+import { useRef, useState } from 'react';
+import './index.css';
+import API from '../../api';
+import cookie from '../../cookie';
+
+function UserOnboard(props) {
+
+
+  return (
+    <div className='userOnboard'>
+    </div>
+  )
+}
+
+export default UserOnboard;
